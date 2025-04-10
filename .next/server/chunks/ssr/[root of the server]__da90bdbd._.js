fb7eebe55e74eab1446a08ca4b7eb938c757fb3b
Jomari Hinayon <hinayonjomari@gmail.com>
2025-04-10 16:26:11 +0800

    refactor: move service account keys to env file

--- .next/server/chunks/ssr/[root of the server]__da90bdbd._.js
+++ .next/server/chunks/ssr/[root of the server]__da90bdbd._.js
@@ -256,7 +256,6 @@ const setSession = async ()=>{
 const clearSession = ()=>{
     // Clear all auth-related cookies
     document.cookie = 'session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; secure';
-    // Clear any other auth-related state here
     localStorage.removeItem('user');
     sessionStorage.clear();
 };
